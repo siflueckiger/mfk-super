92c2e7d5ecf1f5e8eea3f776793a262c20c1e1c1
Simon Flückiger <si.flueckiger@gmail.com>
2021-03-15 22:37:11 +0100

    add pipline scripts and .gitignore

---
+++ PIPELINE/Vernissage_pipeline/Readme.md
@@ -0,0 +1,92 @@
+# Readme Vernissage Pipeline
+
+## GPU
+
+Terminal -> nvtop
+
+ * GPU1 sollte angezeigt werden
+
+    * sonst richtig einstecken, einschalten, neustarten
+
+      
+
+## PIPELINE
+
+* run: python3 vernissage_pipeline.py
+
+* drop dir is where the images go
+
+* pipeline.run(): run the pipeline once
+
+* yet to come: pipeline.handle(): run the pipeline at specific condition eg. N images, Time, key input 
+
+  ### Ordnerstruktur
+
+  --
+
+  ### Material
+  
+* Photostation
+
+   * Photobox mit raspberry pi netzstecker
+   * TV
+   * HDMI Kabel kurz
+   * Buzzer
+   * Steckleiste mind. 4
+
+ * Büro
+
+    * Lenovo Laptop und Netzstecker, Etherner Adapter?
+    * ext. GPU inkl alle Kabel
+    * Router inkl Netzstecker
+    * Bürolampe
+
+* Diverses
+
+  * HDMI Kabel mind. 10 Meter
+  * Ethernerkabel 1x lang 1x kurz
+  * Farbiges Klebeband für Markierungen Wand und Boden
+   * Verlängerungskabel
+   * Zweifachstecker
+   * Ducktape
+   * Kabelbinder
+
+# Todos
+
+- Photobox bauen (Flügu) Ende März-> jaja
+- Backup auf HD (Flügu) sofort
+- Zeugs auf Dropbox oder GIT ohne 50GB (Flügu) sofort
+- Preview beim Buzzer mit overlay (Flügu) Ende März -> Done
+- Ni an Diashow erinnern (Flügu) Mitte April
+- Buzzer antirutsch (Flügu)  -> Teppich klepebeand
+- Diashow (Ni) Mitte April 
+  - Skalierung möglich vergössern max. oder in prozent einstellen
+  - bsp wenn X min nichts passiert random aus allen fotos sonst die neusten Y oder so
+- Pipeline handler (Ni)  -> Done
+
+# 
+
+* Color Historram Transfert zwischen Background und Foreground
+  * Haben wir jetzt mit Colorgrading manuel gelöst
+* Error: Gibt kein Errorchecking 
+  * Ist implementiert, noch testen
+* Dims: Falls die Fotos andere Dimensionen haben braucht es diesen Schritt noch. Pipeline akzeptier 1920x1280 
+* Readme
+* Gif wäre schon noch Cool
+
+# Notizen
+
+## 26.02.21
+
+habe pipeline gestartet. dann kam das
+
+```
+---- tensorman run --gpu -- python evaluate.py     --checkpoint ./checkpoints/useForEvaluation/LargerTrainingDataSet_Train2014_and_WIDER_train_and_OI_Challenge_neonMask_epoches_8/     --in-path ./input-image/     --out-path ./pipelineOutput/     --allow-different-dimensions
+"docker" "run" "-u" "1000:1000" "--gpus=all" "-e" "HOME=/project" "-it" "--rm" "-v" "/home/simonflueckiger/Documents/01_MRT/MfK_Vernissage-Super/PIPELINE/fast-style-transfer:/project" "-w" "/project" "tensorflow/tensorflow:latest-gpu" "python" "evaluate.py" "--checkpoint" "./checkpoints/useForEvaluation/LargerTrainingDataSet_Train2014_and_WIDER_train_and_OI_Challenge_neonMask_epoches_8/" "--in-path" "./input-image/" "--out-path" "./pipelineOutput/" "--allow-different-dimensions"
+Unable to find image 'tensorflow/tensorflow:latest-gpu' locally
+latest-gpu: Pulling from tensorflow/tensorflow
+```
+
+hat dann gaaaanz viel abengeladen
+
+-> sollte man eine bestimte tensorflow version benutzen und nicht `tensorflow:latest-gpu`?
---
+++ PIPELINE/Vernissage_pipeline/vernissage_pipeline.py
@@ -0,0 +1,283 @@
+import os
+import shutil
+import subprocess
+import sys
+import time
+
+# Functions
+def makdirIfnotExists(dirName):
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
+
+
+
+
+class Pipeline:
+    maskDir = "./1.Mask/"
+    styleTransfertDir = "./2.StyleTransfert/"
+    applyMaskDir = "./3.ApplyMask/"
+
+
+    def __init__(this,inpDir, drpDir, outpDir):
+
+        #dropDir = "./drop/
+        #imgs = os.listdir(dropDir) 
+        this.dropDir = drpDir
+        this.inputDir = inpDir
+        this.imgs = os.listdir(this.dropDir)
+        this.outputDir = outpDir
+
+
+    def run(this):
+
+        then = time.time()
+        this.MoveImagesToPipeline()
+        this.MoveImagesToRaw()
+        this.CleanUp()
+        print("############ Starting Pipeline ############")
+
+        this.GenerateMask()
+        this.StyleTransfert()
+        this.ApplyMask()
+        #this.CleanUp()
+
+        now = time.time()
+
+        ellapsed = (now-then)
+        minutes = round(ellapsed/60,0)
+        seconds = round(ellapsed%60,2)
+        nImages = len(this.imgs)
+        perImage = ellapsed/nImages
+
+        print("###### It took" ,minutes,"minutes and", seconds, "seconds to run" , nImages , "images.")
+
+        print("###### This is", round(perImage, 2) ,"seconds per image.")
+
+    def MoveImagesToPipeline(this):
+
+        #print("############ There are : ", len(imgs)," images available. Moving 10 of em.")
+        """
+        for i in this.imgs:
+            shutil.move(this.dropDir+i, this.inputDir+i)
+        """
+        try:
+            subprocess.check_call("mv  "+this.dropDir+"/* "+this.inputDir, shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured while copying the images from the shared Directory to the Pipeline..")
+        this.imgs = os.listdir(this.inputDir)
+
+        print("############The following images were passed to the pipeline...")
+        print()
+        print(this.imgs, sep="/n")
+        print()
+    
+    def MoveImagesToRaw(this):
+        try:
+            subprocess.check_call("cp -r "+this.inputDir+"* ./raw/", shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured while copying the images to the raw dir.")
+
+
+    def CleanUp(this):
+
+
+        print("########## Cleaning Up Pipeline\n")
+        mask = os.listdir(this.maskDir)
+        if len(mask) > 0:
+            try: 
+                subprocess.check_call("rm -r ./1.Mask/*", shell=True)
+            except subprocess.CalledProcessError:
+                sys.exit("There was an error, while cleaning up maksDir.")
+
+        style = os.listdir(this.styleTransfertDir)
+        if (len(style) > 0):
+            try: 
+                subprocess.check_call("rm -r ./2.StyleTransfert/*", shell=True)
+            except subprocess.CalledProcessError:
+                sys.exit("There was an error, while cleaning up StyleTransfetDir")
+
+        apply = os.listdir(this.applyMaskDir)
+        if (len(apply) > 0):
+            try: 
+                subprocess.check_call("rm -r ./3.ApplyMask/*", shell=True)
+            except subprocess.CalledProcessError:
+                sys.exit("There was an error, while cleaning up ApplyMaskDir")
+
+
+
+
+    ##########################################
+    ############## First Step ################
+    ##########################################
+
+    def GenerateMask(this):
+        print("############ 1. Mask ############ ")
+
+        pythonFile = "../bgMask/segmentation_pipeline.py"
+
+        maskDirInput = "./1.Mask/Input/"
+        maskDirOutput = "./1.Mask/Output/"
+
+        makdirIfnotExists(maskDirInput)
+        makdirIfnotExists(maskDirOutput)
+
+        for img in this.imgs:
+            shutil.copy(this.inputDir+img,maskDirInput+img)
+
+        try:
+            subprocess.check_call(pythonFile, shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured generating the masks..")
+
+
+    ###########################################
+    ############## Second Step ################
+    ###########################################
+    def StyleTransfert(this):
+
+        styleDirInput = "./2.StyleTransfert/Input/"
+        styleDirOutput = "./2.StyleTransfert/Output/"
+
+        makdirIfnotExists(styleDirInput)
+        makdirIfnotExists(styleDirOutput)
+
+        print()
+        print('###########')
+        print('########### Copying')
+        print('########### from Pipeline input directory')
+        print('########### to Step 2 input directory')
+        print('###########')
+        print()
+
+        for img in this.imgs:
+            shutil.copy(this.inputDir+img, styleDirInput+img)
+
+        print("########### 2. StyleTransfert")
+        print("########### Moving to Directory")
+
+        cdToDir = "cd ../fast-style-transfer/; "
+        pythonFile = "./runEvaluation_pipeline.py"
+
+        try:
+            subprocess.check_call(cdToDir+pythonFile, shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured at styletrasfert stage.")
+
+        print()
+
+        #print("########## Moving back to Pipeline Directory.")
+
+        #subprocess.call("cd ./", shell=True)
+        #subprocess.call("pwd", shell=True)
+        #subprocess.call("ls ", shell=True)
+
+
+    ###########################################
+    ############## Third Step ################
+    ###########################################
+
+    def ApplyMask(this):
+        MaskInputDir = "./3.ApplyMask/MaskInput/"
+        StyledInputDir = "./3.ApplyMask/StyledInput/"
+        ApplyedMaskOutputDir = "./3.ApplyMask/Output/"
+
+        makdirIfnotExists(MaskInputDir)
+        makdirIfnotExists(StyledInputDir)
+        makdirIfnotExists(ApplyedMaskOutputDir)
+
+        ## Output dirs from previous steps
+        maskDirOutput = "./1.Mask/Output/"
+        styleDirOutput = "./2.StyleTransfert/Output/"
+
+        print()
+        print('###########')
+        print('########### Copying')
+        print('########### from Step 1 output directory')
+        print('########### to Step 3 inputMask directory')
+        print('###########')
+        print()
+
+
+        try:
+            subprocess.check_call("cp "+maskDirOutput+"* "+MaskInputDir, shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured copying images to the 3th Step...")
+
+        print()
+        print('###########')
+        print('########### Copying')
+        print('########### from Step 2 output directory')
+        print('########### to Step 3 StyledInput directory')
+        print('###########')
+        print()
+
+
+        try:
+            subprocess.check_call("cp "+styleDirOutput+"* "+StyledInputDir, shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured coping images to the 3th Step..")
+
+        print("########### 3. Apply Mask and add Background Image on Styled Image")
+
+        pythonFile = "python3 ../ApplyMask/applyMask_pipeline.py"
+
+        try:
+            subprocess.check_call(pythonFile, shell=True)
+        except subprocess.CalledProcessError:
+            sys.exit("An error occured runing the applyMask_pipeline skript.")
+
+
+
+        print("########## Copy files to the Pipeline Output Directory")
+
+        subprocess.call("cp "+ ApplyedMaskOutputDir + "/* " + this.outputDir, shell=True)
+
+    def handle(this):
+        cond = False
+        then = time.time()-55
+    
+        while True:
+            imgs = os.listdir(this.dropDir)
+            now = time.time()
+            elapsed = (now-then)
+
+            if elapsed > 60: 
+                #elapsed = 0
+                print("Waiting for images...          ",end = "\r")
+            else:
+                print("Starting pipeline in {}".format(round(60.-elapsed,3)),end='\r')
+            cond = len(imgs) >= 1 and elapsed > 60
+            #print(cond)
+            if cond:
+                this.run()
+                then = time.time()
+
+
+def main():
+    #NUM_IMAGES = len(imgs) #### FOR TESTING !!!!!
+    """
+    if len(imgs) < NUM_IMAGES:
+        print("############ There are less then 10 images. Exiting..")
+        sys.exit()
+        
+    """
+    DROP =  "/run/user/1000/gvfs/smb-share:server=raspberrypi.local,share=mfk-super-share/" # where the new images need to go
+    INPUTDIR =  "./input/" # move to input dir to start prcdocessing
+    OUTPUTDIR = "./output/" # result of whole pipeline
+
+    pipeline = Pipeline(INPUTDIR, DROP, OUTPUTDIR)
+    #while true:
+    #pipeline.CleanUp()
+    
+    #pipeline.run()
+
+    pipeline.handle()
+
+if __name__ == "__main__":
+    main()
+
+###### It took 1.0 minutes and 10.1 seconds to run 11 images.
+###### This is  6.37 seconds per image.
+
+###### It took 8.0 minutes and 37.88 seconds to run 101 images.
+###### This is 4.53 seconds per image.
